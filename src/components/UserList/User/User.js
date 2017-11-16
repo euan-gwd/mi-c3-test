a9fb3ce187102af6ca03fd958efa5b61f4b8a100
euan-gwd <euan@amwic.co.za>
2017-11-16 22:41:34 +0200

    Add Table Styles

--- src/components/UserList/User/User.js
+++ src/components/UserList/User/User.js
@@ -3,16 +3,14 @@ import './User.css';
 
 const User = ({ userInfo }) => {
   return (
-    <table className="">
-      <th className="grid-table-data">
-        <div>{userInfo.name}</div>
-        <div>{userInfo.height}</div>
-        <div>{userInfo.mass}</div>
-        <div>{userInfo.created}</div>
-        <div>{userInfo.edited}</div>
-        <a linkto={userInfo.homeworld}>HomeWorld</a>
-      </th>
-    </table>
+    <div className="grid-table-data">
+      <div>{userInfo.name}</div>
+      <div>{userInfo.height}cm</div>
+      <div>{userInfo.mass}kg</div>
+      <div>{userInfo.created}</div>
+      <div>{userInfo.edited}</div>
+      <a linkto={userInfo.homeworld}>Planet of Origin</a>
+    </div>
   );
 };
 
--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import User from './User/User';
+import './UserList.css';
 
-const UserList = props => {
-  return <div>{props.users.map(user => <User key={user.name} userInfo={user} />)}</div>;
+const UserList = ({ users }) => {
+  return (
+    <div className="table">
+      <div className="grid-table-headers">
+        <div>Name</div>
+        <div>Height</div>
+        <div>Mass</div>
+        <div>Created</div>
+        <div>Edited</div>
+        <div>Planet</div>
+      </div>
+      <div>{users.map(user => <User key={user.name} userInfo={user} />)}</div>
+    </div>
+  );
 };
 
 export default UserList;
