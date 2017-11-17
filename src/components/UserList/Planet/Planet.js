9ca6923ce5ac9b5e8709e743da3a6ef6a253dddd
euan-gwd <euan@amwic.co.za>
2017-11-17 14:54:00 +0200

    save 1

--- src/components/UserList/Planet/Planet.js
+++ src/components/UserList/Planet/Planet.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 class Planet extends Component {
-  state = { planetData: [] };
+  state = { data: [] };
   componentDidMount = () => {
     const url = this.props.url;
-    // axios.get(`https://swapi.co/api/people`).then(res => {
-    //   const planetData = res.data.results;
-    //   this.setState({ planetData });
-    // });
+    axios.get(url).then(res => {
+      this.setState({ data: res.data });
+    });
   };
   render() {
-    return <div />;
+    const { data } = this.state;
+    return (
+      <div>
+        <div className="planet-grid-headers">
+          <div>Name: {data.name}</div>
+          <div>Climate: {data.climate}</div>
+          <div>Terrain: {data.terrain}</div>
+          <div>Diameter: {data.diameter}</div>
+          <div>Gravity: {data.gravity}</div>
+          <div>Orbital Period: {data.orbital_period}</div>
+          <div>Population: {data.population}</div>
+          <div>Rotation Period: {data.rotation_period}</div>
+          <div>Surface Water: {data.surface_water}</div>
+        </div>
+      </div>
+    );
   }
 }
 
