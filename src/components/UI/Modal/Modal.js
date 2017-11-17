6c077f21e370627656336a8b9980d14e4f1f61c7
euan-gwd <euan@amwic.co.za>
2017-11-17 12:51:32 +0200

    Remove inline Styles

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,15 +6,7 @@ const Modal = props => {
   return (
     <div>
       <Backdrop show={props.show} hide={props.hide} />
-      <div
-        className="Modal"
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}
-      >
-        {props.children}
-      </div>
+      <div className={props.show ? 'Modal open' : ' Modal close'}>{props.children}</div>
     </div>
   );
 };
