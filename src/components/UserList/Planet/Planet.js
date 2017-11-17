5af2fd54e24c790f7d3b9591f5477b5de2ac3b90
euan-gwd <euan@amwic.co.za>
2017-11-17 11:36:45 +0200

    Add Modal

---
+++ src/components/UserList/Planet/Planet.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Planet extends Component {
+  state = {};
+  render() {
+    return <div />;
+  }
+}
+
+export default Planet;
--- src/components/UserList/User/User.js
+++ src/components/UserList/User/User.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { format } from 'date-fns';
+import Modal from '../../UI/Modal/Modal';
+import Planet from '../Planet/Planet';
 import './User.css';
 
-const User = ({ userInfo }) => {
-  return (
-    <div className="grid-table-data">
-      <div>{userInfo.name}</div>
-      <div>{userInfo.height}cm</div>
-      <div>{userInfo.mass}kg</div>
-      <div>{format(userInfo.created, 'DD/MM/YYYY HH:mm:ss')}</div>
-      <div>{format(userInfo.edited, 'DD/MM/YYYY HH:mm:ss')}</div>
-      <a link={userInfo.homeworld}>View</a>
-    </div>
-  );
-};
+class User extends Component {
+  state = {};
+  render() {
+    const { userInfo } = this.props;
+    return (
+      <div>
+        <div className="grid-table-data">
+          <div>{userInfo.name}</div>
+          <div>{userInfo.height}cm</div>
+          <div>{userInfo.mass}kg</div>
+          <div>{format(userInfo.created, 'DD/MM/YYYY HH:mm:ss')}</div>
+          <div>{format(userInfo.edited, 'DD/MM/YYYY HH:mm:ss')}</div>
+          <a>View</a>
+        </div>
+        <Modal>
+          <Planet url={userInfo.homeworld} />
+        </Modal>
+      </div>
+    );
+  }
+}
 
 export default User;
