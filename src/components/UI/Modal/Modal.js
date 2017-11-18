96131e7c66813b73592c41072bde0fcfa03e884b
euan-gwd <euan@amwic.co.za>
2017-11-18 22:10:01 +0200

    Refactor Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Backdrop from './Backdrop';
+import styled from 'styled-components';
 import './Modal.css';
 
 const Modal = props => {
   return (
     <div>
       <Backdrop show={props.show} hide={props.hide} />
-      <div className={props.show ? 'Modal open' : ' Modal close'}>{props.children}</div>
+      <ModalContent className={props.show ? 'open' : 'close'}>{props.children}</ModalContent>
     </div>
   );
 };
+
+const ModalContent = styled.div`
+  position: fixed;
+  z-index: 10;
+  background-color: white;
+  width: 50%;
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 1px black;
+  padding: 0.25rem;
+  left: 25vw;
+  top: 25vh;
+  box-sizing: border-box;
+  transition: all 0.3s ease-out;
+`;
 
 export default Modal;
