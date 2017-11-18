14881719724347b371f7829c2153bb09663677ff
euan-gwd <euan@amwic.co.za>
2017-11-18 17:21:12 +0200

    Add Column Sort

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -4,7 +4,7 @@ import User from './User/User';
 import './UserList.css';
 
 class UserList extends Component {
-  state = { users: {}, nextUrl: null, loaded: false };
+  state = { users: {}, nextUrl: null, loaded: false, sortBy: 'name', orderDir: 'asc' };
 
   componentDidMount = () => {
     axios.get(`https://swapi.co/api/people`).then(res => {
@@ -21,6 +21,30 @@ class UserList extends Component {
     }
   };
 
+  handleSortByColumn = id => {
+    function compareValues(key, order = 'asc') {
+      return function(a, b) {
+        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+          return 0;
+        }
+
+        const varA = typeof a[key] === 'string' ? a[key].toUpperCase() : a[key];
+        const varB = typeof b[key] === 'string' ? b[key].toUpperCase() : b[key];
+
+        let comparison = 0;
+        if (varA > varB) {
+          comparison = 1;
+        } else if (varA < varB) {
+          comparison = -1;
+        }
+        return order === 'desc' ? comparison * -1 : comparison;
+      };
+    }
+
+    const sortedUsers = this.state.users.sort(compareValues(id));
+    this.setState({ users: sortedUsers });
+  };
+
   render() {
     return (
       <div className="container">
@@ -31,11 +55,21 @@ class UserList extends Component {
           </button>
         </nav>
         <div className="user-grid-headers">
-          <div>Name</div>
-          <div>Height</div>
-          <div>Mass</div>
-          <div>Created</div>
-          <div>Edited</div>
+          <button className="headerBtn" onClick={this.handleSortByColumn.bind(this, 'name')}>
+            Name
+          </button>
+          <button className="headerBtn" onClick={this.handleSortByColumn.bind(this, 'height')}>
+            Height
+          </button>
+          <button className="headerBtn" onClick={this.handleSortByColumn.bind(this, 'mass')}>
+            Mass
+          </button>
+          <button className="headerBtn" onClick={this.handleSortByColumn.bind(this, 'created')}>
+            Created
+          </button>
+          <button className="headerBtn" onClick={this.handleSortByColumn.bind(this, 'edited')}>
+            Edited
+          </button>
           <div>Planet</div>
         </div>
         {this.state.loaded ? (
